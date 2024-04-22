4bb0675ff7705f6c7bea8eec002a8cdc25f2860c
CandraEliantari <c.candraa12@gmail.com>
2024-04-22 12:44:27 +0700

    commit Rara

---
+++ app/src/main/java/com/example/modul6/DetailContactActivity.java
@@ -0,0 +1,19 @@
+package com.example.modul6;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetailContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_detail_contact);
+    }
+}
